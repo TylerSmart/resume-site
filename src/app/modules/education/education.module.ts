a9dc9cbd8946e7b7eeade6d203adbdc44c4603f6
Tyler Smart <smart@savorymanagement.com>
2022-12-08 11:04:13 -0700

    v1.5.1 - Fix education dependencies and exports

--- src/app/modules/education/education.module.ts
+++ src/app/modules/education/education.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EducationComponent } from './components/education/education.component';
-
-
+import { MaterialModule } from '../material/material.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    EducationComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [EducationComponent],
+  imports: [CommonModule, MaterialModule, SharedModule],
+  exports: [EducationComponent],
 })
-export class EducationModule { }
+export class EducationModule {}
